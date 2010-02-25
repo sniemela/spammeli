b20ee8711da6742a8ee4a0ab9f70c549c7673ace
Simo Niemelä <simo.niemela@gmail.com>
2010-02-26 00:10:50 +0200

    Don't need to rescue just return

--- lib/spammeli/commands/math.rb
+++ lib/spammeli/commands/math.rb
@@ -3,18 +3,15 @@ module Spammeli
     class Math < Spammeli::Command
       def invoke
         formula = params.join
+        unless formula =~ /^[0-9\(\)\+\-\/\*\%\.]+$/
+          return "The formula contains some illegal characters"
+        end
         
         begin
-          unless formula =~ /^[0-9\(\)\+\-\/\*\%\.]+$/
-            raise ArgumentError
-          end
-        
           result = eval(formula)
           raise ZeroDivisionError if result =~ /divided by 0/
-          
+        
           result
-        rescue ArgumentError
-          "The formula may contain some illegal characters"
         rescue ZeroDivisionError
           "Zero division :/"
         end
