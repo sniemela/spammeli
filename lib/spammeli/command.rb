8800c3ecda9d9239f877dcea49c1a0321d664f0b
Simo Niemelä <simo.niemela@gmail.com>
2010-02-25 15:02:39 +0200

    Command should now its command name

--- lib/spammeli/command.rb
+++ lib/spammeli/command.rb
@@ -1,16 +1,20 @@
 module Spammeli
   class Command
-    attr_reader :name, :params
+    attr_reader :params
     
     class << self  
       def inherited(klass)
-        ::Spammeli::CommandRegistry.register(klass.to_s.downcase, klass)
+        ::Spammeli::CommandRegistry.register(klass.command_name, klass)
         super
       end
     end
     
     def initialize(params = [])
       @params = params
+    end
+    
+    def self.command_name
+      self.to_s.downcase
     end
   end
 end
