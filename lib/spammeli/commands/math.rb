bdabd9eb0dc1ab3cf711a2ecc0637b1d70c2d35d
Simo Niemelä <simo.niemela@gmail.com>
2010-02-28 17:02:31 +0200

    Support math functions

--- lib/spammeli/commands/math.rb
+++ lib/spammeli/commands/math.rb
@@ -1,13 +1,24 @@
 module Spammeli
   module Commands
-    class Math < Spammeli::Command
+    class Mathematic < Spammeli::Command
+      MATH_FUNCTIONS = %w(
+        acos acosh asin asinh atan atan2
+        atanh cosh erf erfc exp frexp hypot
+        ldexp log log10 sinh sqrt tan tanh sin cos
+      ).sort
+      
       def invoke
         formula = params.join
-        unless formula =~ /^[0-9\(\)\+\-\/\*\%\.]+$/
+        
+        return functions if formula == 'functions'
+        
+        math_methods = MATH_FUNCTIONS.join('|')
+        unless formula =~ /^[0-9\(\)\+\-\/\*\%\.(#{math_methods})]+$/
           return "The formula contains some illegal characters"
         end
         
         begin
+          formula = formula.gsub(/(#{math_methods})/, 'Math.\1')
           result = eval(formula)
           raise ZeroDivisionError if result =~ /divided by 0/
         
@@ -16,6 +27,12 @@ module Spammeli
           "Zero division :/"
         end
       end
+      
+      def functions
+        MATH_FUNCTIONS.join(', ')
+      end
     end
   end
+  
+  CommandRegistry.rename!(:mathematic, :math)
 end
--- spec/spammeli/commands/math_spec.rb
+++ spec/spammeli/commands/math_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
-describe (Math = Spammeli::Commands::Math) do
+describe (Mathematic = Spammeli::Commands::Mathematic) do
   it "should calculate numbers" do
     formula = "(2 + 2) * 3 - 2".split(' ')
     
-    Math.new(formula).invoke.should == 10
+    Mathematic.new(formula).invoke.should == 10
+  end
+  
+  it "should support math functions" do
+    formula = "sqrt(1) * 2".split(' ')
+    Mathematic.new(formula).invoke.should == 2.0
+  end
+  
+  it "should complain about illegal characters" do
+    formula = "Math.sqrt(1) * 2".split(' ')
+    Mathematic.new(formula).invoke.should == "The formula contains some illegal characters"
   end
 end
