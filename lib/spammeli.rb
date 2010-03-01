4b3945994b7c7fedf01bbf67cc1b5d71f1c933a1
Simo Niemelä <simo.niemela@gmail.com>
2010-03-01 20:57:28 +0200

    Make spammeli configurable

--- lib/spammeli.rb
+++ lib/spammeli.rb
@@ -3,6 +3,7 @@
 module Spammeli
   autoload :Command,         'spammeli/command'
   autoload :CommandRegistry, 'spammeli/command_registry'
+  autoload :Config,          'spammeli/config'
   autoload :Input,           'spammeli/input'
   autoload :Irc,             'spammeli/irc'
   autoload :Output,          'spammeli/output'
---
+++ lib/spammeli/config.rb
@@ -0,0 +1,30 @@
+require 'yaml'
+
+module Spammeli
+  class Config
+    @@config = nil
+  
+    class << self
+      def load
+        @@config ||= YAML.load(File.open(File.expand_path('../../../config.yaml', __FILE__)))
+      end
+
+      def load!
+        @@config = nil
+        load
+      end
+
+      def get(key)
+        load['spammeli'][key.to_s]
+      end
+      
+      def channels
+        if chans = get(:channels)
+          chans.to_a.compact.map { |c| "#" + c.to_s }
+        else
+          []
+        end
+      end
+    end
+  end
+end
