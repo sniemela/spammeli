24dcafbeeca9400c7344ffce6f7053a823008ed0
Simo Niemelä <simo.niemela@gmail.com>
2010-03-01 22:55:34 +0200

    Show help message if lastfm user command has no params

--- lib/spammeli/commands/lastfm.rb
+++ lib/spammeli/commands/lastfm.rb
@@ -6,10 +6,35 @@ module Spammeli
     class Lastfm < Spammeli::Command
       def invoke
         return help if params.empty?
+        
+        type, name, option = params[0], params[1], params[2]
+        
+        case type
+        when 'user'
+          LastfmUser.new(name, option).invoke
+        else
+          help
+        end
       end
       
       def help
         "Syntax: !lastfm [user/artist] [name] [options]"
+      end
+    end
+    
+    class LastfmUser
+      attr_reader :name, :option
+      
+      def initialize(name, option)
+        @name, @option = name, option
+      end
+      
+      def invoke
+        return help unless name && option
+      end
+      
+      def help
+        "Syntax: !lastfm user [name] [options]. Options: recent"
       end
     end
   end
--- spec/spammeli/commands/lastfm_spec.rb
+++ spec/spammeli/commands/lastfm_spec.rb
@@ -4,4 +4,8 @@ describe (Lastfm = Spammeli::Commands::Lastfm) do
   it "should show help message if parameters are empty" do
     Lastfm.new([]).invoke.should == 'Syntax: !lastfm [user/artist] [name] [options]'
   end
+  
+  it "should show help message for user param" do
+    Lastfm.new(['user']).invoke.should == 'Syntax: !lastfm user [name] [options]. Options: recent'
+  end
 end
