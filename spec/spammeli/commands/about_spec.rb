a5b181275047d726cad714142a3e7d81b58f4e4a
Simo Niemelä <simo.niemela@gmail.com>
2010-03-04 23:55:13 +0200

    Show version info in the about text

--- spec/spammeli/commands/about_spec.rb
+++ spec/spammeli/commands/about_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 describe (About = Spammeli::Commands::About) do
   it "should show copyright and contributors" do
-    About.new.invoke.should == 'Spammeli Irc Bot, powered by Ruby. Copyright 2010 Simo Niemelä.'
+    About.new.invoke.should == "Spammeli #{Spammeli::Version::STRING}, powered by Ruby. Copyright 2010 Simo Niemelä."
   end
 end
