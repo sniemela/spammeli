9b0cb8dcf63c651c81be6838f4fc112333344ca2
Simo Niemelä <simo.niemela@gmail.com>
2010-08-13 00:43:17 +0300

    Remove some specs

--- spec/spammeli/commands/about_spec.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-require 'spec_helper'
-
-describe (About = Spammeli::Commands::About) do
-  it "should show copyright and contributors" do
-    About.new.invoke.should == "Spammeli #{Spammeli::Version::STRING}, powered by Ruby. Copyright 2010 Simo Niemelä."
-  end
-end
